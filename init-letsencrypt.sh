f905c45c133c7eb36c506fda6e6651cfa023a169
Alessio Campanelli <alessio.campanelli@altran.it>
2021-08-26 16:26:14 +0200

    nginx configuration

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(maac-stampings.de www.maac-stampings.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="alecampa2003@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
